dcae82eb64f75040fd1eb618475d580f9ca9edaf
leobtt <leonarbat8@gmail.com>
2022-04-25 21:17:42 -0300

    .

--- firestore-access/src/acess.ts
+++ firestore-access/src/acess.ts
@@ -6,22 +6,5 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
-console.log('working...')
-
-const db = admin.firestore()
-
-const cat = 'ITOEAA62kslSoXbogMrf'
-const catRef = db.collection('categories').doc(cat)
-
-
-
-const products = db.collection('products').where('category','array-contains',catRef).get()
-products.then(snapshot => {
-  console.log('------------------ empty', snapshot.empty)
-    snapshot.forEach(snap => {
-      console.log( snap.data())
-    })
-  })
-
 
 export { admin }
