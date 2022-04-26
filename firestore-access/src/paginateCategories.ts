a3fc4b3177f93ddce3533d16544d3f74d3d05495
leobtt <leonarbat8@gmail.com>
2022-04-26 12:32:31 -0300

    paginate

---
+++ firestore-access/src/paginateCategories.ts
@@ -0,0 +1,28 @@
+import { admin } from './acess'
+
+const db = admin.firestore()
+
+const pageSize = 1
+
+db.collection('categories')
+  .orderBy('category')
+  .limit(pageSize + 1)
+  .startAfter('Eletrônicos')
+  .get()
+  .then( snapshot => {
+    let total = 0
+    snapshot.forEach( snap => {
+      if(total < pageSize) {
+        console.log(' id ',snap.id,' => ', snap.data() )
+      }
+      total++
+    })
+
+    if(total > pageSize){
+      console.log('tem uma próxima página')
+    } else {
+      console.log(' Não há próxima página')
+    }
+  })
+
+
